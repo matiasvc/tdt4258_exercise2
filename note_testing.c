e35e3a6f47935b9e433350caa9f8ded9872f6346
Magnus Karlsen <github43@thixateo.net>
2016-10-14 12:45:22 +0200

    Added Melody Methods

--- note_testing.c
+++ note_testing.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 
+#include "efm32gg.h"
+#include "audioMixer.h"
+
 typedef enum
 {
 	 E2, //82.4
@@ -8,13 +11,69 @@ typedef enum
 	 G3, //196.0
 	 B3, //246.9
    E4 //329.6
-} GuitarNotes;
+} Notes;
 
-AudioEffect GetFrequency(GuitarNotes gnote, float duration)
+typedef struct
+{
+  float from;
+  float to;
+} DurationTuple;
+
+typedef struct
+{
+  AudioEffect* audioEffectPointers[6];
+} AudioEffectPoiters;
+
+typedef struct
+{
+  AudioEffect AudioEffects[50];
+  DurationTuple Durations[50];
+  uint16_t NoteCount;
+} Melody;
+
+uint16_t melody_initialize(Melody *melody, uint16_t noteCount);
+AudioEffectPoiters melody_getRange(Melody *melody, float from, float to);
+AudioEffect GetFrequency(Notes note, float duration);
+
+uint16_t melody_initialize(Melody *melody, uint16_t noteCount)
+{
+  melody->NoteCount = noteCount;
+  return noteCount;
+}
+
+void melody_addNote(Melody *melody, Notes note, float duration, float from, float to, uint16_t position)
+{
+  AudioEffect audioeffect = GetFrequency(note, duration);
+  melody->AudioEffects[position] = audioeffect;
+  melody->Durations[position] = (DurationTuple) { .from = from, .to = to};
+}
+
+// returnerer liste med pointers med audioeffects, null-terminert
+AudioEffectPoiters melody_getRange(Melody *melody, float from, float to)
+{
+  AudioEffectPoiters audioEffectPointers;
+  uint8_t arrayCount = 0;
+
+  for(int i = 0; i < melody->NoteCount; i++)
+  {
+    if ((melody->Durations[i].from >= from && melody->Durations[i].from <= to)
+      ||(melody->Durations[i].to >= from && melody->Durations[i].to <= to))
+    {
+      audioEffectPointers.audioEffectPointers[arrayCount++] = &(melody->AudioEffects[i]);
+    }
+  }
+
+  if (arrayCount > 5) { arrayCount = 5; }
+  audioEffectPointers.audioEffectPointers[arrayCount] = '\0';
+
+  return audioEffectPointers;
+}
+
+AudioEffect GetFrequency(Notes note, float duration)
 {
   float frequency;
 
-  switch (gnote)
+  switch (note)
 	{
 		case E2 : { frequency = 82; } break;
 		case A2 : { frequency = 110; } break;
@@ -34,4 +93,6 @@ AudioEffect GetFrequency(GuitarNotes gnote, float duration)
 	effect.decayFrequency = 0;
 	effect.frequencyAttackTransition = EASE_IN;
 	effect.frequencyDecayTransition = EASE_OUT;
+
+  return effect;
 }
